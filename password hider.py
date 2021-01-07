3e4fdd0ed87f13f18dc6fde68d37ba1acd844f4f
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-07 22:09:45 +0530

    Add files via upload

---
+++ password hider.py
@@ -0,0 +1,10 @@
+        
+        import getpass
+
+        password=getpass.getpass(prompt="Enter the password : ")
+        if password == "python":
+            print("success")
+        else:
+            print("Please type correctly...")
+            
+                    
