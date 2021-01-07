61ffb7a7bdf234497e886f18c9fff89c12e1a15b
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-07 22:14:15 +0530

    Update password hider.py

--- password hider.py
+++ password hider.py
@@ -1,10 +1,7 @@
-        
-        import getpass
+import getpass
 
-        password=getpass.getpass(prompt="Enter the password : ")
-        if password == "python":
-            print("success")
-        else:
-            print("Please type correctly...")
-            
-                    
+password=getpass.getpass(prompt="Enter the password : ")        # whatever u type in output window will not be visible to your eyes
+if password == "python":               # U can give anything u want to set as a password        
+    print("success")
+else:
+    print("Please type correctly...")   # if u type wrongly means this print statement will be print in the output window
